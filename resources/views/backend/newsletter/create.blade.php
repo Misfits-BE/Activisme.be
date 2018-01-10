470f6bc24fb4b8c4fffde8ce7255788871813e05
vagrant <vagrant@activisme-be>
2018-01-10 15:03:52 +0000

    Start on view

--- resources/views/backend/newsletter/create.blade.php
+++ resources/views/backend/newsletter/create.blade.php
@@ -2,7 +2,21 @@
 
 @section('content')
     <div class="container my-4">
-        {{ Breadcrumbs::register('newsletter-create') }}
+        {{ Breadcrumbs::render('newsletter-create') }}
         @include('flash::message')
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header"> {{-- Card header --}}
+                        <i class="fa fa-plus"></i> Nieuwsbrief toevoegen
+                    </div> {{-- /// End card header --}}
+
+                    <div class="card-body"> {{-- Card body --}}
+
+                    </div> {{-- /// Card body --}}
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
