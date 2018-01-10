919f3cedc38cf0ec2df77845363fcafcf8870575
Tjoosten <Topairy@gmail.com>
2018-01-10 06:27:15 +0100

    Implement missing docblocks calendar controller

--- app/Http/Controllers/Frontend/CalendarController.php
+++ app/Http/Controllers/Frontend/CalendarController.php
@@ -7,15 +7,36 @@ use Illuminate\View\View;
 use ActivismeBe\Repositories\CalendarRepository;
 use ActivismeBe\Http\Controllers\Controller;
 
+/**
+ * Class CalendarController
+ *
+ * @author    Tim Joosten <tim@activisme.be>
+ * @copyright 2018 Tim Joosten
+ * @package   ActivismeBe\Http\Controllers\Frontend
+ */
 class CalendarController extends Controller
 {
-    private $calendarRepository; 
+    /**
+     * @var CalendarRepository $calendarRepository
+     */
+    private $calendarRepository;
 
+    /**
+     * CalendarController constructor.
+     *
+     * @param  CalendarRepository $calendarRepository Abstractie laag tussen controller, logica; database.
+     * @return void
+     */
     public function __construct(CalendarRepository $calendarRepository) 
     {
         $this->calendarRepository = $calendarRepository;
     }
 
+    /**
+     * De index controller voor de front-end kalender.
+     *
+     * @return \Illuminate\View\View
+     */
     public function index(): View
     {
         return view('frontend.calendar.index', [
