3046823f068a63724b6414f966a8444f9334e54e
Tjoosten <Topairy@gmail.com>
2018-01-10 06:29:20 +0100

    Register hasher

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Contracts\Hashing\Hasher;
 
 trait CreatesApplication
 {
@@ -14,8 +15,8 @@ trait CreatesApplication
     public function createApplication()
     {
         $app = require __DIR__.'/../bootstrap/app.php';
-
         $app->make(Kernel::class)->bootstrap();
+        $app->make(Hasher::class)->setRounds(4);
 
         return $app;
     }
