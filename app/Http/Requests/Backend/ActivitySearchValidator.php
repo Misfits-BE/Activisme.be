e0b4ff4c3fc671c53603a10fb085151a8bded8cb
Tim Joosten <Tjoosten@users.noreply.github.com>
2018-01-12 10:52:55 +0100

    Fix #37 | Implementatie zoek methode voor de logs (#40)
    * Implementatie activiteit zoek weergave
    * Fix #37 | Implementatie zoek methode voor de logs

---
+++ app/Http/Requests/Backend/ActivitySearchValidator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ActivismeBe\Http\Requests\Backend;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ActivitySearchValidator extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check() && auth()->user()->hasRole('admin');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return ['term' => 'max:120|required'];
+    }
+}
